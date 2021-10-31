52367a91e1997fd580f734c5e096d222a2d2514a
Andrew Yu <andrewyu209@gmail.com>
2021-10-31 19:34:44 -0400

    update planner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,32 @@
+function updateTimeBlock() {
+    // Add class to change block color for Past/Present/Future
+    $(".hour").each(function() {
+        var hours = moment($(this).text().trim(),'LT');
+        var duration = Math.ceil(moment.duration(hours.diff(moment())).asHours());
+        if(parseInt(duration)< 0) {
+            $(this).next().addClass("past");
+        }
+        else if(parseInt(duration) === 0)
+        {
+            $(this).next().addClass("present");
+        }
+        else {
+            $(this).next().addClass("future");
+        }
+    })
+}
+
+function updatePlanner(e) {
+    var description = $(e.target).siblings('.description').val();
+    var timeId = $(e.target).parent('.time-block').attr('id');
+    console.log(timeId, " ", description);
+    localStorage.setItem(timeId, description);
+}
+
 function main() {
     $("#currentDay").text(moment().format('dddd, LL'));
+    updateTimeBlock();
+    $('.saveBtn').on('click', updatePlanner);
 };
 
 main();
